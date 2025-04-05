d1d8a41f5f5900f6033795c93f4c46fbf7b86e05
vikrantsrgc <145971065+vikrantsrgc@users.noreply.github.com>
2025-04-05 13:49:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-function makeid(l) {
-  // write your code here
+function makeid(length) {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = '';
+  
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    result += characters[randomIndex];
+  }
+  
+  return result;
 }
+
 
 // Do not change the code below.
 const l = prompt("Enter a number.");
